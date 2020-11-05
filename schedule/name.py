c996478f430d7735d10fda044413635e141bf5b6
hufuture <48757669+hufuture@users.noreply.github.com>
2020-11-05 14:58:33 +0800

    Create name.py

---
+++ schedule/name.py
@@ -0,0 +1,30 @@
+class DataName:
+    w = "w"
+    n = "n"
+    m = "m"
+    ops = "ops"
+    prt = "prt"
+
+
+class GaName:
+    pop_size = "pop_size"
+    rate_crossover = "rate_crossover"
+    rate_mutation = "rate_mutation"
+    operator_crossover = "operator_crossover"
+    operator_mutation = "operator_mutation"
+    operator_crossover_mac = "operator_crossover_mac"
+    operator_mutation_mac = "operator_mutation_mac"
+    operator_selection = "operator_selection"
+    stop_max_generation = "stop_max_generation"
+    stop_max_stay = "stop_max_stay"
+    function_objective = "function_objective"
+
+    pop_info = "pop_info"
+    pop_objective = "pop_objective"
+    pop_fitness = "pop_fitness"
+    generation_info = "generation_info"
+    generation_objective = "generation_objective"
+    generation_runtime = "generation_runtime"
+
+    pop_osc = "pop_osc"
+    pop_wkc = "pop_wkc"
